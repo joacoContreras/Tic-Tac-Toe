b9993e609ae615769154ca5802f112de5e94f77a
joacoContreras <cjoaquin835@gmail.com>
2026-08-31 18:48:40 +0000

    feature: gameboard IIFE

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 // ==========================================================================
-// Entities & State (OOP)
+// Entities & State
 // ==========================================================================
+const gameboard = (() => {
+    let positions = ["", "", "", "", "", "", "", "", ""];
+
+    const add = (index, marker) => {
+        // Validamos que la posicion este vacia antes de agregar la ficha
+        if (positions[index] === "") {
+            positions[index] = marker;
+            return true; // Jugada exitosa
+        }
+        return false; // Si el casillero ya estaba ocupado
+    };
+
+    const getState = () => {
+        return[...positions]; // Retornamos una copia del arreglo
+    };
+
+    return { add, getState};
+})();
+
+function createPlayer(name, record) {
+    const {} = createPlayer(name);
+
+    const getRecord = () => record;
+    const increaseRecord = () => { record++; };
+    return {
+        name,
+        getRecord,
+        increaseRecord,
+    };
+}
+
+const gameController = (() => {
+
+})
+
+const displayController = (() => {
+
+})
